733a1b37f58d35c3297f04a838cb99d6cb815182
soooochan <nowee018@naver.com>
2023-09-19 13:54:44 +0900

    Add : Toggle

---
+++ parking-lot/src/pages/Myinfo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {useState} from "react";
+
+const Myinfo = () => {
+    return(
+        <div>
+            <h2>hello</h2>
+        </div>
+    )
+}
+
+export default Myinfo;
--- parking-lot/src/pages/Parkinglot.js
+++ parking-lot/src/pages/Parkinglot.js
@@ -1,30 +1,71 @@
 import React, {useState} from "react";
+import Myinfo from "./Myinfo";
 import "../styles/Parkinglot.css";
+
+
+
+
 
 class Home extends React.Component {
 
-  nameSection(name){
-    return(
-      <div className="mainname">
-       {name}
-      </div>
-    )
-
+  constructor(props){
+    super(props);
+    this.state={
+      visible:false
+    }
   }
+  
+
+  
 
   render() {
+    const { visible } = this.state; // 상태에서 visible 값을 가져옴
+    
+    
+
     return(
 
       <div>
       
           <img src="img/parking_detection.png" alt="logo" />  
    
-         
-        <div>
-            {this.nameSection("Parking Area Type Occupied Sattus")}
+        {/* 토글 버튼 */}  
+        <div className="Select">
+        
+        <div className="button">  
+          <button className="mainname" onClick = {() => {
+            this.setState({ visible: !visible }); // 상태 업데이트
+          }}
+          >
+            {visible ? "Parking Area Type Occupied Sattus" : "Parking Area Type Occupied Sattus"}
+          </button>
+
+          {visible && <Myinfo />} {/* 조건부 렌더링 */}
+
+        </div>
+        
+        {/* 조건부 렌더링 */}
+
+        </div>
+           
+        <div className="Select">
+          <div className="button">  
+            <button className="mainname" onClick = {() => {
+              this.setState({ visible: !visible }); // 상태 업데이트
+            }}
+            >
+              {visible ? "Parking Area Type Occupied Sattus" : "Parking Area Type Occupied Sattus"}
+            </button>
+
+            {visible && <Myinfo />} {/* 조건부 렌더링 */}
+
+          </div>
+        
         </div>
       
       </div>
+
+      
 
     );
   }
